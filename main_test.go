bf7d085ed468b757a0f0e313cb8bc06d97292de7
Ahern <zeyuan.li@shopee.com>
2025-06-28 14:44:18 +0800

    feat: skip list

--- main_test.go
+++ main_test.go
@@ -16,23 +16,29 @@ func TestLevelDb(t *testing.T) {
 	defer db.Close()
 
 	key1 := []byte("key_1")
+	key2 := []byte("key_2")
 	value1 := []byte("val_1")
+	value2 := []byte("val_2")
 
 	/*
-	写入简要流程：
-	1、flush函数：写入流量控制
-	2、writeJournal写入WAL文件（未实现落盘）
-	3、写入memory
+		写入简要流程：
+		1、flush函数：写入流量控制
+		2、writeJournal写入WAL文件（未实现落盘）
+		3、写入memory
 	*/
 	if err := db.Put(key1, value1, nil); err != nil {
 		t.Fatal(err)
 	}
 
+	if err := db.Put(key2, value2, nil); err != nil {
+		t.Fatal(err)
+	}
 
-	val, err := db.Get(key1, nil)
+	// val, err := db.Get(key1, nil)
+	val, err := db.Get([]byte("key"), nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	assert.Equal(t,value1, val)
+	assert.Equal(t, value1, val)
 }
